9b459df9f3c345914f2879f6bcb2e95cc880340a
Vernon Mensah <vernonmensah@gmail.com>
2018-02-13 21:07:33 -0500

    Added functionality to add data to EventDataCampaign

--- controllers/saveEventDataController.js
+++ controllers/saveEventDataController.js
@@ -3,24 +3,39 @@ const EventDataModel = require('../models/eventDataModel');
 const saveEventData = (eventType, customerEmail, campaignEventDataId) => {
   console.log('CAMPAIGNEVENTDATAID, EVENTTYPE, CUSTOMEREMAIL', campaignEventDataId, eventType, customerEmail)
 
-    // EventDataModel.findById(campaignEventDataId)
-    //     .then((data) => {
-    //         if (data) {
-    //             console.log(data);
-    //             // data.campaigns = [...data.campaigns, campaign]
-    //             // return data.save();
-    //         }
-    //         else {
-    //             return response.send('Nope.');
-    //         }
-    //     })
-    //     .then(res => {
-    //       console.log(res);
-    //       return response.status(201).send('Added.');
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
-    //     });
+    EventDataModel.findById(campaignEventDataId)
+        .then((data) => {
+            if (data) {
+                console.log(data.eventType);
+                saveEventType(data, eventType, customerEmail);
+                return data.save();
+            }
+            else {
+                response.send('Nope.');
+                return Promise.reject(response.send('Nope.'));
+            }
+        })
+        .then(res => {
+          console.log(res);
+          return response.status(201).send('Added event data.');
+        })
+        .catch((err) => {
+            response.send(err);
+            console.log(err);
+        });
 };
 
+const saveEventType = (eventDataCampaign, eventType, email) => {
+
+    if (eventDataCampaign[eventType]) {
+
+      eventDataCampaign[eventType]['emails'] = [...eventDataCampaign[eventType]['emails'], email];
+      return eventDataCampaign.save();
+
+    }
+
+    else {
+      return Promise.reject(response.send('Nope.'));
+    }
+}
 module.exports = { saveEventData };
